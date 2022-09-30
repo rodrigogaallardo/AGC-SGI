38f94c59a72837e3bcac82929041e5e27f1bc1de
Facundo Forcada <31261216+FacundoForcada@users.noreply.github.com>
2022-09-30 09:56:15 -0300

    Revert "Asosa - Modulo de Administracion de Tareas de una Solicitud"

--- SGI/Model/DDLGrouped.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SGI.Model
-{
-    public class DDLGrouped
-    {
-        public int DataValueField { get; set; }
-        public string DataTextField { get; set; }
-        public string DataGroupByField { get; set; }
-    }
-}
