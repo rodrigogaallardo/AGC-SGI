cd3078082bcaf1ac7b0cdd04a570b68f8816f22f
luis espindola <luisesp27@live.com.ar>
2024-01-29 14:03:53 -0300

    Fixed, ahora siempre muestra los archivos de las encomiendas

--- SGI/Operaciones/AdministrarArchivosDeUnaSolicitud.aspx.designer.cs
+++ SGI/Operaciones/AdministrarArchivosDeUnaSolicitud.aspx.designer.cs
@@ -96,6 +96,15 @@ namespace SGI.Operaciones
         protected global::System.Web.UI.WebControls.Label txtEstadoValor;
 
         /// <summary>
+        /// Control txtUsuarioCaratuladorValor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txtUsuarioCaratuladorValor;
+
+        /// <summary>
         /// Control txtUsuarioValor.
         /// </summary>
         /// <remarks>
